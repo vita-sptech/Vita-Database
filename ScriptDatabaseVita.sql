d833c520389b0501f7e93eed3f2d73e81c4edac1
kevenhistolino <keven.histolino@sptech.school>
2023-09-27 08:58:43 -0300

    Adição da entidade endereço e atualização de alguns campos

--- ScriptDatabaseVita.sql
+++ ScriptDatabaseVita.sql
@@ -3,25 +3,39 @@ CREATE DATABASE IF NOT EXISTS `VITA`;
 
 USE `VITA` ;
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Hospital` (
-  `Id` INT NOT NULL,
-  `Empresa` VARCHAR(45) NOT NULL,
-  `Responsável` VARCHAR(45) NOT NULL,
-  `Telefone` VARCHAR(45) NOT NULL,
-  `Endereço` VARCHAR(45) NOT NULL,
-  `EmailEmpresa` VARCHAR(45) NOT NULL,
-  `CNPJ` VARCHAR(45) NOT NULL,
-  `CodEmpresa` CHAR(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS `VITA`.`Endereco` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Estado` VARCHAR(45) NOT NULL,
+  `Cidade` VARCHAR(45) NOT NULL,
+  `CEP` VARCHAR(45) NOT NULL,
+  `Logradouro` VARCHAR(45) NOT NULL,
+  `NumEndereco` VARCHAR(45) NOT NULL,
+  `Complemento` VARCHAR(45) NULL,
   PRIMARY KEY (`Id`)
   )AUTO_INCREMENT = 100;
 
 
+CREATE TABLE IF NOT EXISTS `VITA`.`Hospital` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `FkEndereco` INT NOT NULL,
+  `Empresa` VARCHAR(45) NOT NULL,
+  `Telefone` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  `CNPJ` VARCHAR(45) NOT NULL,
+  `CodEmpresa` CHAR(32) NOT NULL,
+  PRIMARY KEY (`Id`),
+  CONSTRAINT `fk_Hospital_Endereco1`
+    FOREIGN KEY (`FkEndereco`)
+    REFERENCES `VITA`.`Endereco` (`Id`)
+)AUTO_INCREMENT = 200;
+
+
 CREATE TABLE IF NOT EXISTS `VITA`.`Funcionário` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `FkEmpresa` INT NOT NULL,
-  `EmailFuncionario` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
   `Senha` VARCHAR(45) NOT NULL,
-  `Usuario` VARCHAR(45) NOT NULL,
+  `Nome` VARCHAR(45) NOT NULL,
   `CPF` CHAR(11) NOT NULL,
   `Telefone` CHAR(11) NOT NULL,
   `Funcao` VARCHAR(45) NOT NULL,
@@ -29,24 +43,23 @@ CREATE TABLE IF NOT EXISTS `VITA`.`Funcionário` (
   CONSTRAINT `fk_Cadastro-Funcionário_Cadastro-Empresa1`
     FOREIGN KEY (`FkEmpresa`)
     REFERENCES `VITA`.`Hospital` (`Id`)
-    )AUTO_INCREMENT = 200;
+)AUTO_INCREMENT = 300;
 
 
 CREATE TABLE IF NOT EXISTS `VITA`.`Maquina` (
-  `Id` INT NOT NULL,
+  `Id` INT NOT NULL AUTO_INCREMENT,
   `FkEmpresa` INT NOT NULL,
   `Apelido` VARCHAR(45) NOT NULL,
   `Responsavel` VARCHAR(45) NOT NULL,
-  `JanelasSistema` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Id`),
   CONSTRAINT `fk_Maquinas_Cadastro-Empresa1`
     FOREIGN KEY (`FkEmpresa`)
     REFERENCES `VITA`.`Hospital` (`Id`)
-    )AUTO_INCREMENT = 300;
+)AUTO_INCREMENT = 400;
 
 
 CREATE TABLE IF NOT EXISTS `VITA`.`Hardware` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `FkMaquina` INT NOT NULL,
   `UsoProcessador` VARCHAR(45) NOT NULL,
   `TempProcessador` VARCHAR(45) NOT NULL,
@@ -60,11 +73,11 @@ CREATE TABLE IF NOT EXISTS `VITA`.`Hardware` (
   CONSTRAINT `fk_Hardware_Maquinas1`
     FOREIGN KEY (`FkMaquina`)
     REFERENCES `VITA`.`Maquina` (`Id`)
-    )AUTO_INCREMENT = 400;
+) AUTO_INCREMENT = 500;
 
 
 CREATE TABLE IF NOT EXISTS `VITA`.`Historico-Incidentes` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `FkMaquina` INT NOT NULL,
   `Componente` VARCHAR(45) NOT NULL,
   `Data` DATETIME NOT NULL,
@@ -72,4 +85,4 @@ CREATE TABLE IF NOT EXISTS `VITA`.`Historico-Incidentes` (
   CONSTRAINT `fk_Historico-Incidentes_Maquinas1`
     FOREIGN KEY (`FkMaquina`)
     REFERENCES `VITA`.`Maquina` (`Id`)
-    )AUTO_INCREMENT = 500;
+) AUTO_INCREMENT = 600;
