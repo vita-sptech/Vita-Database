8105cf0616ce058f89d66c871262eaaafeeb548e
Leopls <leonardobento233@gmail.com>
2023-10-02 13:16:18 -0300

    Modificações Script + Adição dos Inserts

--- ScriptDatabaseVita.sql
+++ ScriptDatabaseVita.sql
@@ -1,88 +1,97 @@
-
-CREATE DATABASE IF NOT EXISTS `VITA`;
-
-USE `VITA` ;
-
-CREATE TABLE IF NOT EXISTS `VITA`.`Endereco` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Estado` VARCHAR(45) NOT NULL,
-  `Cidade` VARCHAR(45) NOT NULL,
-  `CEP` VARCHAR(45) NOT NULL,
-  `Logradouro` VARCHAR(45) NOT NULL,
-  `NumEndereco` VARCHAR(45) NOT NULL,
-  `Complemento` VARCHAR(45) NULL,
-  PRIMARY KEY (`Id`)
-  )AUTO_INCREMENT = 100;
+DROP DATABASE IF EXISTS VITA;
+CREATE DATABASE IF NOT EXISTS VITA;
 
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Hospital` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `FkEndereco` INT NOT NULL,
-  `Empresa` VARCHAR(45) NOT NULL,
-  `Telefone` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  `CNPJ` VARCHAR(45) NOT NULL,
-  `CodEmpresa` CHAR(32) NOT NULL,
-  PRIMARY KEY (`Id`),
-  CONSTRAINT `fk_Hospital_Endereco1`
-    FOREIGN KEY (`FkEndereco`)
-    REFERENCES `VITA`.`Endereco` (`Id`)
+USE VITA ;
+
+CREATE TABLE IF NOT EXISTS Hospital (
+  Id INT NOT NULL AUTO_INCREMENT,
+  Nome VARCHAR(255) NOT NULL,
+  Telefone VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  CNPJ VARCHAR(255) NOT NULL,
+  CodHospital VARCHAR(255) NOT NULL,
+  PRIMARY KEY (Id)
 )AUTO_INCREMENT = 200;
 
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Funcionário` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `FkEmpresa` INT NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  `Senha` VARCHAR(45) NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  `CPF` CHAR(11) NOT NULL,
-  `Telefone` CHAR(11) NOT NULL,
-  `Funcao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`, `FkEmpresa`),
-  CONSTRAINT `fk_Cadastro-Funcionário_Cadastro-Empresa1`
-    FOREIGN KEY (`FkEmpresa`)
-    REFERENCES `VITA`.`Hospital` (`Id`)
+CREATE TABLE IF NOT EXISTS Endereco (
+  Id INT NOT NULL AUTO_INCREMENT,
+  FkHospital INT NOT NULL,
+  Estado VARCHAR(255) NOT NULL,
+  Cidade VARCHAR(255) NOT NULL,
+  CEP VARCHAR(255) NOT NULL,
+  Logradouro VARCHAR(255) NOT NULL,
+  NumEndereco VARCHAR(255) NOT NULL,
+  Complemento VARCHAR(255) NULL,
+  FOREIGN KEY (FkHospital) REFERENCES Hospital (Id),
+  PRIMARY KEY (Id)
+  )AUTO_INCREMENT = 100;
+
+CREATE TABLE IF NOT EXISTS Funcionario (
+  Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  FkHospital INT NOT NULL,
+  Email VARCHAR(255) NOT NULL,
+  Senha VARCHAR(255) NOT NULL,
+  Nome VARCHAR(255) NOT NULL,
+  CPF CHAR(255) NOT NULL,
+  Telefone CHAR(255) NOT NULL,
+  Funcao VARCHAR(255) NOT NULL,
+  FOREIGN KEY (FkHospital) REFERENCES Hospital (Id)
 )AUTO_INCREMENT = 300;
 
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Maquina` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `FkEmpresa` INT NOT NULL,
-  `Apelido` VARCHAR(45) NOT NULL,
-  `Responsavel` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`),
-  CONSTRAINT `fk_Maquinas_Cadastro-Empresa1`
-    FOREIGN KEY (`FkEmpresa`)
-    REFERENCES `VITA`.`Hospital` (`Id`)
+CREATE TABLE IF NOT EXISTS Maquina (
+  Id INT NOT NULL AUTO_INCREMENT,
+  FkEmpresa INT NOT NULL,
+  Apelido VARCHAR(255) NOT NULL,
+  Responsavel VARCHAR(255) NOT NULL,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (FkEmpresa) REFERENCES Hospital (Id)
 )AUTO_INCREMENT = 400;
 
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Hardware` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `FkMaquina` INT NOT NULL,
-  `UsoProcessador` VARCHAR(45) NOT NULL,
-  `TempProcessador` VARCHAR(45) NOT NULL,
-  `FreqProcessador` VARCHAR(45) NOT NULL,
-  `UsoMemoria` VARCHAR(45) NOT NULL,
-  `TempMemoria` VARCHAR(45) NOT NULL,
-  `FreqMemoria` VARCHAR(45) NOT NULL,
-  `TotalDisco` VARCHAR(45) NOT NULL,
-  `UsoDiscco` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`, `FkMaquina`),
-  CONSTRAINT `fk_Hardware_Maquinas1`
-    FOREIGN KEY (`FkMaquina`)
-    REFERENCES `VITA`.`Maquina` (`Id`)
+CREATE TABLE IF NOT EXISTS Hardware(
+  Id INT NOT NULL AUTO_INCREMENT,
+  FkMaquina INT NOT NULL,
+  UsoProcessador VARCHAR(255) NOT NULL,
+  TempProcessador VARCHAR(255) NOT NULL,
+  FreqProcessador VARCHAR(255) NOT NULL,
+  UsoMemoria VARCHAR(255) NOT NULL,
+  TempMemoria VARCHAR(255) NOT NULL,
+  FreqMemoria VARCHAR(255) NOT NULL,
+  TotalDisco VARCHAR(255) NOT NULL,
+  UsoDisco VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id, FkMaquina),
+  FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
 ) AUTO_INCREMENT = 500;
 
 
-CREATE TABLE IF NOT EXISTS `VITA`.`Historico-Incidentes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `FkMaquina` INT NOT NULL,
-  `Componente` VARCHAR(45) NOT NULL,
-  `Data` DATETIME NOT NULL,
-  PRIMARY KEY (`id`, `FkMaquina`),
-  CONSTRAINT `fk_Historico-Incidentes_Maquinas1`
-    FOREIGN KEY (`FkMaquina`)
-    REFERENCES `VITA`.`Maquina` (`Id`)
+CREATE TABLE IF NOT EXISTS HistoricoIncidentes (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  FkMaquina INT NOT NULL,
+  Componente VARCHAR(255) NOT NULL,
+  DtIncidente DATETIME NOT NULL,
+  FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
 ) AUTO_INCREMENT = 600;
+
+
+DESC HOSPITAL;
+INSERT INTO HOSPITAL VALUES 
+(NULL,"Alberto Einstein", "11961707443","alberteinstein@hospital.com","60.765.823/0001-30","824fd9d8-6134-11ee-8c99-0242ac120002"),
+(NULL,"HOSPITAL MUNICIPAL NOSSA SENHORA DA LAPA", "3438131231","hospitalapa@hospital.com","18.278.069/0001-47","7e6de880-6136-11ee-8c99-0242ac120002");
+SELECT * FROM HOSPITAL;
+
+DESC ENDERECO;
+INSERT INTO ENDERECO VALUES
+(NULL,200,"Sao Paulo","Sao Paulo","01414952","Rua do hospital albert eintein","900","Do outro lado da rua é o outro lado da rua do hospital"),
+(NULL,201,"Sao Paulo","Sao Paulo","01445652","Rua do hospital da lapa","1452","");
+SELECT * FROM ENDERECO;
+
+DESC FUNCIONARIO;
+INSERT INTO FUNCIONARIO VALUES
+(null,200,"lucassantos@gmail.com","senha@123","Lucão","12312312","123123123","Gerente");
+
+DESC MAQUINA;
+INSERT INTO MAQUINA VALUES (null,200,"Maquina 1 Atendimento","David");
+select * from MAQUINA;
