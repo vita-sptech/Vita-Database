e1dadc578808012465897d11fa72978610ce8b35
Leopls <leonardobento233@gmail.com>
2023-10-04 14:50:50 -0300

    Adição tabela Processo + Disco

--- ScriptDatabaseVita.sql
+++ ScriptDatabaseVita.sql
@@ -66,6 +66,26 @@ CREATE TABLE IF NOT EXISTS Hardware(
   FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
 ) AUTO_INCREMENT = 500;
 
+CREATE TABLE IF NOT EXISTS Processo (
+Id INT primary key NOT NULL auto_increment,
+FkMaquina INT NOT NULL,
+Nome VARCHAR (255),
+dtCaptura DATETIME,
+Threads INT,
+usoMemoriaRam VARCHAR(255),
+FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
+);
+
+CREATE TABLE IF NOT EXISTS Disco (
+Id INT primary key NOT NULL auto_increment,
+FkMaquina INT NOT NULL,
+dtCaptura DATETIME,
+ArmazenamentoTotal INT,
+ArmazenamentoLivre VARCHAR(255),
+FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
+);
+
+
 
 CREATE TABLE IF NOT EXISTS HistoricoIncidentes (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -73,27 +93,26 @@ CREATE TABLE IF NOT EXISTS HistoricoIncidentes (
   Componente VARCHAR(255),
   DtIncidente DATETIME,
   FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
-) AUTO_INCREMENT = 600;
+);
 
 
-DESC HOSPITAL;
+-- DESC HOSPITAL;
 INSERT INTO HOSPITAL VALUES 
 (NULL,"Alberto Einstein", "11961707443","alberteinstein@hospital.com","60.765.823/0001-30","824fd9d8-6134-11ee-8c99-0242ac120002"),
 (NULL,"HOSPITAL MUNICIPAL NOSSA SENHORA DA LAPA", "3438131231","hospitalapa@hospital.com","18.278.069/0001-47","7e6de880-6136-11ee-8c99-0242ac120002");
 SELECT * FROM HOSPITAL;
 
-DESC ENDERECO;
+-- DESC ENDERECO;
 INSERT INTO ENDERECO VALUES
 (NULL,200,"Sao Paulo","Sao Paulo","01414952","Rua do hospital albert eintein","900","Do outro lado da rua é o outro lado da rua do hospital"),
 (NULL,201,"Sao Paulo","Sao Paulo","01445652","Rua do hospital da lapa","1452","");
 SELECT * FROM ENDERECO;
 
-DESC FUNCIONARIO;
+-- DESC FUNCIONARIO;
 INSERT INTO FUNCIONARIO VALUES
 (null,200,"lucassantos@gmail.com","senha@123","Lucão","12312312","123123123","Gerente");
 
-DESC MAQUINA;
+-- DESC MAQUINA;
 INSERT INTO MAQUINA VALUES (null,200,"Maquina 1 Atendimento","David");
-select * from MAQUINA;
+-- select * from MAQUINA;
 
-select * from hardware;
