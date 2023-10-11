2de0c5ebdca7883a16982285ed4774f26c958383
leopls07 <leonardobento233@gmail.com>
2023-10-10 21:30:50 -0300

    Mudanças na maquina e no funcionario

--- ScriptDatabaseVita.sql
+++ ScriptDatabaseVita.sql
@@ -32,76 +32,75 @@ CREATE TABLE IF NOT EXISTS Funcionario (
   email VARCHAR(255) NOT NULL,
   senha VARCHAR(255) NOT NULL,
   nome VARCHAR(255) NOT NULL,
-  cpf CHAR(255) NOT NULL,
-  telefone CHAR(255) NOT NULL,
+  telefone VARCHAR(255) NOT NULL,
   funcao VARCHAR(255) NOT NULL,
   FOREIGN KEY (fkHospital) REFERENCES Hospital (id)
 )AUTO_INCREMENT = 300;
 
 
 CREATE TABLE IF NOT EXISTS Maquina (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  fkEmpresa INT NOT NULL,
+  uuid VARCHAR(255) PRIMARY KEY  ,
+  fkHospital INT NOT NULL,
   apelido VARCHAR(255),
   responsavel VARCHAR(255),
-  FOREIGN KEY (fkEmpresa) REFERENCES Hospital (id)
+  FOREIGN KEY (fkHospital) REFERENCES Hospital (id)
 )AUTO_INCREMENT = 400;
 
 CREATE TABLE IF NOT EXISTS processoRegistro (
 id INT primary key NOT NULL auto_increment,
-fkMaquina INT NOT NULL,
+fkMaquina  VARCHAR(255) NOT NULL,
 nome VARCHAR (255),
 dtRegistro DATETIME,
 threads INT,
 usoMemoriaRam VARCHAR(255),
-FOREIGN KEY (fkMaquina) REFERENCES Maquina (id)
+FOREIGN KEY (fkMaquina) REFERENCES Maquina (uuid)
 );
 
 CREATE TABLE IF NOT EXISTS discoRegistro (
 id INT primary key auto_increment,
-fkMaquina INT NOT NULL,
+fkMaquina VARCHAR(255) NOT NULL,
 modelo VARCHAR(255),		
 dtRegistro DATETIME,
 armazenamentoTotal VARCHAR(255),
 armazenamentoLivre VARCHAR(255),
-FOREIGN KEY (fkMaquina) REFERENCES Maquina (id)
+FOREIGN KEY (fkMaquina) REFERENCES Maquina (uuid)
 );
 
 CREATE TABLE IF NOT EXISTS cpuRegistro (
 id INT primary key auto_increment,
-fkMaquina INT NOT NULL,
+fkMaquina VARCHAR(255) NOT NULL,
 dtRegistro DATETIME,
 temperatura VARCHAR(50),
 usoPorcentagem VARCHAR(50),
-FOREIGN KEY (fkMaquina) REFERENCES Maquina (id)
+FOREIGN KEY (fkMaquina) REFERENCES Maquina (uuid)
 );
 
 CREATE TABLE IF NOT EXISTS memoriaRegistro(
 id INT primary key auto_increment,
-fkMaquina int,
+fkMaquina VARCHAR(255),
 dtRegistro DATETIME,
 qtdTotal VARCHAR(45),
 usoMemoria VARCHAR(45),
-FOREIGN KEY (fkMaquina) references Maquina (id)
+FOREIGN KEY (fkMaquina) references Maquina (uuid)
 );
 
 CREATE TABLE IF NOT EXISTS sistemaRegistro(
 id INT PRIMARY KEY auto_increment,
-fkMaquina int,
+fkMaquina VARCHAR(255),
 dtRegistro DATETIME,
 tempoDeAtividadeSistema VARCHAR(45),
 qtdDispositivosUsb INT,
-FOREIGN KEY (fkMaquina) references Maquina (id)
+FOREIGN KEY (fkMaquina) references Maquina (uuid)
 );
 
 
 
 CREATE TABLE IF NOT EXISTS HistoricoIncidentes (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  FkMaquina INT NOT NULL,
+  FkMaquina VARCHAR(255) NOT NULL,
   Componente VARCHAR(255),
   DtIncidente DATETIME,
-  FOREIGN KEY (FkMaquina) REFERENCES Maquina (Id)
+  FOREIGN KEY (FkMaquina) REFERENCES Maquina (uuid)
 );
 
 -- DESC HOSPITAL;
@@ -118,10 +117,11 @@ SELECT * FROM ENDERECO;
 
 -- DESC FUNCIONARIO;
 INSERT INTO FUNCIONARIO VALUES
-(null,200,"lucassantos@gmail.com","senha@123","Lucão","12312312","123123123","Gerente");
+(null,200,"lucassantos@gmail.com","senha@123","Lucão","12312312"	,"Gerente"),
+(null,200,"leo@gmail.com","senha@123","Leo","12312312","Suporte");
 
 -- DESC MAQUINA;
-INSERT INTO MAQUINA VALUES (null,200,"Maquina 1 Atendimento","David");
+INSERT INTO MAQUINA VALUES ("FB3E6C80-DC2E-11EA-8327-38E31E5A3200",200,"Maquina 1 Atendimento","David");
 -- select * from MAQUINA;
 
 select * from discoregistro;
@@ -131,5 +131,13 @@ select * from cpuregistro;
 select * from memoriaregistro;
 
 select * from sistemaregistro;
+
+-- SELECT * FROM MAQUINA M JOIN HOSPITAL H ON M.FKHOSPITAL = H.ID JOIN FUNCIONARIO F ON H.ID = F.FKHOSPITAL WHERE F.EMAIL = "leo@gmail.com" AND F.SENHA = "senha@123";
+SELECT * FROM MAQUINA WHERE UUID = 400;
+SELECT * FROM FUNCIONARIO WHERE email = "leo@gmail.com" AND senha = "senha@123";
+
+desc maquina;
+
+ -- delete from maquina where uuid = 'FB3E6C80-DC2E-11EA-8327-38E31E5A3200'
 
 
